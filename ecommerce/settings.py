52edcf4ccc5b951dcc1a44fe265fc9cacc244be3
Md. Omar Faruk <iamomar022@gmail.com>
2020-08-21 14:02:27 +0600

    Models

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u07zl7x(&dkcgr9w1l21j_1e)*pa0^ksf25zchu@yl7(bcz2yp'
+SECRET_KEY = 'u07zl7x(&dkcgr9womar1l21j_1e)*pa0^ksffaruk25zchu@yl7(bcz2yp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -80,9 +80,19 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+'''
 
 
 # Password validation

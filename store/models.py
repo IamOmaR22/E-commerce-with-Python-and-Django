aeda13a6a850a94520915e305ecb8ccf802579b4
Md. Omar Faruk <iamomar022@gmail.com>
2020-08-23 20:39:24 +0600

    Product Image Field

--- store/models.py
+++ store/models.py
@@ -16,9 +16,18 @@ class Product(models.Model):
 	name = models.CharField(max_length=200)
 	price = models.FloatField()
 	digital = models.BooleanField(default=False,null=True, blank=True)
+	image = models.ImageField(null=True, blank=True) 
 
 	def __str__(self):
 		return self.name
+
+	@property
+	def imageURL(self):
+		try:
+			url = self.image.url
+		except:
+			url = ''
+		return url
 
 class Order(models.Model):
 	customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)

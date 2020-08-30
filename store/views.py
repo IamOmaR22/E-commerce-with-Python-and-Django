6b7041204a09a2d8886dbacf4f723a8b0ed1122b
Md. Omar Faruk <iamomar022@gmail.com>
2020-08-31 01:04:20 +0600

    Render cart data

--- store/views.py
+++ store/views.py
@@ -9,8 +9,17 @@ def store(request):
 	return render(request, 'store/store.html', context)
 
 def cart(request):
-     context = {}
-     return render(request, 'store/cart.html', context)
+
+    if request.user.is_authenticated:
+         customer = request.user.customer
+         order, created = Order.objects.get_or_create(customer=customer, complete=False)
+         items = order.orderitem_set.all()
+    else:
+         #Create empty cart for now for non-logged in user
+         items = []
+
+    context = {'items':items}
+    return render(request, 'store/cart.html', context)
 
 def checkout(request):
       context = {}

eeafa56dc7612629ec33dde4b4fdb007260cda76
Md. Omar Faruk <iamomar022@gmail.com>
2020-08-31 02:08:15 +0600

    User Cart

--- store/views.py
+++ store/views.py
@@ -17,10 +17,21 @@ def cart(request):
     else:
          #Create empty cart for now for non-logged in user
          items = []
+         order = {'get_cart_total':0, 'get_cart_items':0}
 
-    context = {'items':items}
+    context = {'items':items, 'order':order}
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
-      context = {}
-      return render(request, 'store/checkout.html', context)
+      
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        #Create empty cart for now for non-logged in user
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+
+    context = {'items':items, 'order':order}
+    return render(request, 'store/checkout.html', context)

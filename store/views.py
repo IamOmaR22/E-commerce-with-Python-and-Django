891d7a73058bfca3f65853d63657ed4de757fb8b
Md. Omar Faruk <iamomar022@gmail.com>
2020-08-31 12:55:12 +0600

    updateUserOrder()

--- store/views.py
+++ store/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
 from .models import * 
 
 # Create your views here.
@@ -35,3 +37,6 @@ def checkout(request):
 
     context = {'items':items, 'order':order}
     return render(request, 'store/checkout.html', context)
+
+def updateItem(request):
+	return JsonResponse('Item was added', safe=False)

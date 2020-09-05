9e121b5e396a46cfe0f6984d223d1b66fb9facf9
Md. Omar Faruk <iamomar022@gmail.com>
2020-09-05 20:11:35 +0600

    Confirm Total

--- store/views.py
+++ store/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import json
+import datetime
 
 from .models import * 
 
@@ -84,8 +85,28 @@ def updateItem(request):
 		
 	
 def processOrder(request):
-    # print('Data:',request.body)
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        total = float(data['form']['total'])
+        order.transaction_id = transaction_id
+
+        if total == order.get_cart_total:
+            order.complete = True
+        order.save()    
+
+    else:
+        print('User is not logged in')      
+
     return JsonResponse('Payment submitted..', safe=False)
+
+		
+	
+
+    
 	
 
 	

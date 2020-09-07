327670ee88318ef8aea91fb50cde23c57ddca4e9
Md. Omar Faruk <iamomar022@gmail.com>
2020-09-07 13:32:51 +0600

    Render cart Total

--- store/views.py
+++ store/views.py
@@ -34,9 +34,18 @@ def cart(request):
         cartItems = order.get_cart_items
     else:
         #Create empty cart for now for non-logged in user
+        try:
+            cart = json.loads(request.COOKIES['cart'])
+        except:
+            cart = {}
+            print('CART:', cart)			
+			
         items = []
         order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
         cartItems = order['get_cart_items']
+
+        for i in cart:
+            cartItems += cart[i]['quantity']			
 
     context = {'items':items, 'order':order, 'cartItems':cartItems}
     return render(request, 'store/cart.html', context)

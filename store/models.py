d7e2920daa7c24de81f359a00fc21c50936b0246
Md. Omar Faruk <iamomar022@gmail.com>
2020-09-07 12:19:52 +0600

    DecimalField()

--- store/models.py
+++ store/models.py
@@ -14,7 +14,7 @@ class Customer(models.Model):
 
 class Product(models.Model):
 	name = models.CharField(max_length=200)
-	price = models.FloatField()
+	price = models.DecimalField(max_digits=7, decimal_places=2)
 	digital = models.BooleanField(default=False,null=True, blank=True)
 	image = models.ImageField(null=True, blank=True) 
 

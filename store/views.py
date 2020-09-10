562b7c47967bb3963d4986d56429a331f7b1713b
Md. Omar Faruk <iamomar022@gmail.com>
2020-09-10 13:12:53 +0600

    try except

--- store/views.py
+++ store/views.py
@@ -45,30 +45,33 @@ def cart(request):
         cartItems = order['get_cart_items']
 
         for i in cart:
-            cartItems += cart[i]['quantity']
+            try:
+                cartItems += cart[i]['quantity']
 
-            product = Product.objects.get(id=i)
-            total = (product.price * cart[i]['quantity'])
+                product = Product.objects.get(id=i)
+                total = (product.price * cart[i]['quantity'])
 
-            order['get_cart_total'] += total
-            order['get_cart_items'] += cart[i]['quantity']
+                order['get_cart_total'] += total
+                order['get_cart_items'] += cart[i]['quantity']
 
-            item = {
-                'id':product.id,
-				'product':{
+                item = {
                     'id':product.id,
-                    'name':product.name, 
-                    'price':product.price, 
-				    'imageURL':product.imageURL,
-                }, 
-                'quantity':cart[i]['quantity'],
-				'digital':product.digital,
-                'get_total':total,
-            }
-            items.append(item)
+                    'product':{
+                        'id':product.id,
+                        'name':product.name, 
+                        'price':product.price, 
+                        'imageURL':product.imageURL,
+                    }, 
+                    'quantity':cart[i]['quantity'],
+                    'digital':product.digital,
+                    'get_total':total,
+                }
+                items.append(item)
 
-            if product.digital == False:
-                order['shipping'] = True				        
+                if product.digital == False:
+                    order['shipping'] = True
+            except:
+                pass                        				        
 						
     context = {'items':items, 'order':order, 'cartItems':cartItems}
     return render(request, 'store/cart.html', context)
